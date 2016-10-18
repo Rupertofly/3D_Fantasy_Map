626acb35cd667b95d86464f6eb2a365d6dcb62f5
Rupertofly <rupertofly@gmail.com>
2016-10-19 10:20:21 +1100

    sides code written

--- Web/Threes_testing.js
+++ Web/Threes_testing.js
@@ -3,9 +3,83 @@ var camera;
 var renderer;
 var cube;
 var water;
+var locH, locV; // location of the circle
+var circleColor = 255; // color of the circle
 //debugger;
+var tsinceb = 50;
+var outnum = 3;
 var timet;
 var aniid;
+var bbyte;
+var tsinces = 100;
+var eledges;
+var Ntri;
+xxmap = function(n, start1, stop1, start2, stop2) {
+    return ((n - start1) / (stop1 - start1)) * (stop2 - start2) + start2;
+};
+var inData;
+var serial; // variable to hold an instance of the serialport library
+var portName = 'COM3'; // fill in your serial port name here
+function setup() {
+    serial = new p5.SerialPort(); // make a new instance of the serialport library
+    serial.on('list', printList); // set a callback function for the serialport list event
+    serial.on('connected', serverConnected); // callback for connecting to the server
+    serial.on('open', portOpen); // callback for the port opening
+    serial.on('data', serialEvent); // callback for when new data arrives
+    serial.on('error', serialError); // callback for errors
+    serial.on('close', portClose); // callback for the port closing
+
+    serial.list(); // list the serial ports
+    serial.open(portName); // open a serial port
+    serial.write('x');
+}
+// get the list of ports:
+function printList(portList) {
+    // portList is an array of serial port names
+    for (var i = 0; i < portList.length; i++) {
+        // Display the list the console:
+        println(i + " " + portList[i]);
+    }
+}
+
+function serverConnected() {
+    println('connected to server.');
+}
+
+function portOpen() {
+    println('the serial port opened.');
+}
+
+
+function serialError(err) {
+    println('Something went wrong with the serial port. ' + err);
+}
+
+function portClose() {
+    println('The serial port closed.');
+}
+
+function serialEvent() {
+    // read a string from the serial port
+    // until you get carriage return and newline:
+    var inString = serial.readStringUntil('\n');
+    //check to see that there's actually a string there:
+    if (inString.length > 0) {
+        if (inString !== 'hello') { // if you get hello, ignore it
+            var sensors = split(inString, ','); // split the string on the commas
+            if (sensors.length > 3) { // if there are three elements
+                locH = xmap(sensors[0], 250, 410, 0, width); // element 0 is the locH
+                locV = xmap(sensors[1], 0, 1023, 0, 1); // element 1 is the locV
+                circleColor = 255 - (sensors[2] * 255); // element 2 is the button
+                bbyte = sensors[3];
+            }
+
+        }
+        serial.write(outnum);
+        tsinces = 0;
+        //debugger;
+    }
+}
 var defextent = {
     width: 2,
     height: 2
@@ -19,7 +93,7 @@ var ggui = new dat.GUI({
 });
 
 function generateUneroded(ext) {
-    var mesh = generateGoodMesh(3000, ext);
+    var mesh = generateGoodMesh(90000, ext);
     var h = add(slope(mesh, randomVector(4)),
         cone(mesh, 0.5),
         mountains(mesh, 200, 0.15));
@@ -52,15 +126,15 @@ var createGroupedArray = function(arr, chunkSize) {
     return groups;
 };
 var geopara = {
-    npts: 16000,
+    npts: 240,
     extent: defextent
 };
 var geom = generateUneroded(defextent);
 geom = doErosion(geom, 0.05);
-console.log(geom.mesh.vxs);
+//console.log(geom.mesh.vxs);
 var geomtri = Delaunay.triangulate(geom.mesh.vxs);
 var geotris = createGroupedArray(geomtri, 3);
-console.log(geotris);
+//console.log(geotris);
 var text2 = document.createElement('div');
 text2.style.position = 'absolute';
 //text2.style.zIndex = 1;    // if you still don't see the label, try uncommenting this
@@ -97,17 +171,78 @@ function init() {
     scene.add(light);
     var geometry = new THREE.Geometry();
     var gvxs = geom.mesh.vxs;
+    var thext = geom.mesh.extent;
+    var edge_index = [
+        [],
+        [],
+        [],
+        []
+    ];
+    var edge_points = [
+        [],
+        [],
+        [],
+        []
+    ];
     for (var i = 0; i < geom.length; i++) {
-        var h = -1*geom[i];
+        var h = -1 * geom[i];
+
         //h = Math.max(Math.min(h, 0.1), -0.1);
         //console.log(h);
-        geometry.vertices.push(new THREE.Vector3(gvxs[i][0] - 1, gvxs[i][1] - 1, h));
+        geometry.vertices.push(new THREE.Vector3(gvxs[i][0] - (thext.width / 2), gvxs[i][1] - (thext.width / 2), h));
+        if (gvxs[i][0] <= 0) {
+            edge_index[0].push(i);
+        }
+        if (gvxs[i][1] <= 0) {
+            edge_index[1].push(i);
+        }
+        if (gvxs[i][1] >= thext.width) {
+            edge_index[2].push(i);
+        }
+        if (gvxs[i][0] >= thext.height) {
+            edge_index[3].push(i);
+        }
     }
     for (var j = 0; j < geotris.length; j++) {
         var t = geotris[j];
         geometry.faces.push(new THREE.Face3(t[0], t[1], t[2]));
     }
-    var pgeo = new THREE.PlaneBufferGeometry(2, 2);
+    geometry.vertices.push(new THREE.Vector3(-1, 1, 2));
+    geometry.vertices.push(new THREE.Vector3(-1, -1, 2));
+    edge_index[0].push(geom.length);
+    edge_index[0].push(geom.length + 1);
+    console.log(geometry.vertices[geom.length + 1]);
+    for (var v = 0; v < edge_index[0].length; v++) {
+        var vert = geometry.vertices[edge_index[0][v]];
+        edge_points[0].push({
+            x: vert.y,
+            y: vert.z,
+            i: v
+        });
+    }
+    edge_points[0].sort(function(a, b) {
+        return a.x - b.x;
+    });
+    var sortedpoints = [];
+    for (var s = 0; s < edge_points[0].length; s++) {
+        var ll = edge_points[0][s];
+        sortedpoints.push([ll.x, ll.y]);
+    }
+    sortedpoints = [].concat.apply([], sortedpoints);
+
+    console.log(sortedpoints);
+    Ntri = earcut(sortedpoints);
+    Ntri = createGroupedArray(Ntri,3);
+    for (var c = 0; c < Ntri.length; c++) {
+        var ftri = Ntri[c];
+        var ta = edge_points[0];
+        var tp1 = ta[ftri[0]].i;
+        var tp2 = ta[ftri[1]].i;
+        var tp3 = ta[ftri[2]].i;
+        geometry.faces.push(new THREE.Face3(edge_index[0][tp1],edge_index[0][tp2],edge_index[0][tp3]));
+    }
+    console.log(edge_points[0]);
+    var pgeo = new THREE.PlaneBufferGeometry(3, 3);
     /*var material = new THREE.MeshPhongMaterial({
         color: 0xdddddd,
         shading: THREE.FlatShading,
@@ -127,7 +262,8 @@ function init() {
         fragmentShader: $('#fragmentshader').text(),
         shading: THREE.FlatShading,
         lights: true,
-        uniforms: uniforms
+        uniforms: uniforms,
+        side: THREE.DoubleSide
     });
     var watermaterial = new THREE.MeshPhongMaterial({
         color: 0x13436b,
@@ -153,23 +289,36 @@ function init() {
     //water.position.y = 0.1;
     window.scene = scene;
     window.THREE = THREE;
+    eledges = edge_points;
     render();
 }
 init();
 
 function render() {
-    text2.innerHTML = Math.max.apply(null, geom) + ',' + Math.min.apply(null, geom);
-    text2.style.top = 200 + 'px';
+    text2.innerHTML = cube.rotation.z + ',' + circleColor + ',' + locH + ',' + locV + ',' + outnum + ',' + bbyte;
+    text2.style.top = 60 + 'px';
     text2.style.left = 200 + 'px';
     document.body.appendChild(text2);
+    tsinces++;
     //debugger;
     aniid = requestAnimationFrame(render);
     //timet += 0.5;
     //camera.rotation.x -= 0.001*Math.PI;
     //water.rotation.z += 0.001;
-    cube.rotation.z += 0.001;
-    water.rotation.z += 0.001;
-
+    var rot = Math.floor(locH || 1) * 0.1;
+    cube.rotation.z += 0.02 * rot;
+    water.rotation.z += 0.02 * rot;
+    var floored = ((((Math.abs(cube.rotation.z / (Math.PI * 2))) % 1) * 256));
+    outnum = Math.floor(floored, 0.0, 1.0, 0, 256);
+    var heyo = locV || 1;
+    //debugger;
+    camera.rotation.x = heyo * Math.radians(-45);
+    if (tsinces > 60) {
+        serial.write(outnum); // send a byte requesting more serial data
+        tsinces = 0;
+    } else {
+        tsinces++;
+    }
     renderer.render(scene, camera);
 }
 
